d1e1a757e9b4b63e6c9af91fcdb2e90d0d27f8f4
huukhuong <huukhuong.it@gmail.com>
2022-06-30 23:22:08 +0700

    add react-native-vector-icons

--- src/screens/HomeScreen/HomeScreen.styles.ts
+++ src/screens/HomeScreen/HomeScreen.styles.ts
@@ -6,7 +6,23 @@ const styles = StyleSheet.create({
     backgroundColor: "#0D0D0D"
   },
   appBar: {
-
+    height: 75,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 24,
+    // backgroundColor: "#ccc"
+  },
+  appBarTitle: {
+    color: '#fff',
+    fontSize: 24,
+    flex: 1,
+  },
+  appBarButton: {
+    width: 40,
+    height: 40,
+    backgroundColor: "#0F211D",
+    marginStart: 14,
+    borderRadius: 15
   }
 });
 
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,8 +1,9 @@
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParams } from '../../../App';
 import styles from './HomeScreen.styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 type Props = NativeStackScreenProps<RootStackParams>;
 
@@ -11,6 +12,13 @@ const HomeScreen = ({ navigation, route }: Props) => {
     <SafeAreaView style={styles.container}>
       {/* APPBAR */}
       <View style={styles.appBar}>
+        <Text style={styles.appBarTitle}>WhatsUp</Text>
+        <TouchableOpacity activeOpacity={.7} style={styles.appBarButton}>
+          <Icon name='facebook' size={18} color="#fff" />
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={.7} style={styles.appBarButton}>
+
+        </TouchableOpacity>
       </View>
       {/* END APPBAR */}
     </SafeAreaView>
