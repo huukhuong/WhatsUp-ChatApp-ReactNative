c033ff98fbcdb5dcc20c07ade32d81e830f452ed
huukhuong <huukhuong.it@gmail.com>
2022-07-01 08:32:45 +0700

    changed icon

--- src/screens/HomeScreen/HomeScreen.styles.ts
+++ src/screens/HomeScreen/HomeScreen.styles.ts
@@ -6,23 +6,33 @@ const styles = StyleSheet.create({
     backgroundColor: "#0D0D0D"
   },
   appBar: {
-    height: 75,
+    height: 90,
     flexDirection: 'row',
     alignItems: 'center',
     paddingHorizontal: 24,
     // backgroundColor: "#ccc"
   },
+  userAvatar: {
+    width: 45,
+    height: 45,
+    borderRadius: 30,
+    resizeMode: "cover",
+    marginEnd: 10
+  },
   appBarTitle: {
     color: '#fff',
     fontSize: 24,
+    fontWeight: 'bold',
     flex: 1,
   },
   appBarButton: {
     width: 40,
     height: 40,
     backgroundColor: "#0F211D",
-    marginStart: 14,
-    borderRadius: 15
+    marginStart: 10,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
 
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,9 +1,10 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import { View, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParams } from '../../../App';
 import styles from './HomeScreen.styles';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { default as Ionicons } from 'react-native-vector-icons/Ionicons';
+import { default as FontAwesome } from 'react-native-vector-icons/FontAwesome';
 
 type Props = NativeStackScreenProps<RootStackParams>;
 
@@ -12,12 +13,25 @@ const HomeScreen = ({ navigation, route }: Props) => {
     <SafeAreaView style={styles.container}>
       {/* APPBAR */}
       <View style={styles.appBar}>
+        <Image
+          source={{ uri: "https://cafefcdn.com/thumb_w/650/203337114487263232/2022/3/3/photo1646280815645-1646280816151764748403.jpg" }}
+          style={styles.userAvatar} />
         <Text style={styles.appBarTitle}>WhatsUp</Text>
-        <TouchableOpacity activeOpacity={.7} style={styles.appBarButton}>
-          <Icon name='facebook' size={18} color="#fff" />
+        <TouchableOpacity
+          activeOpacity={.7}
+          style={styles.appBarButton}>
+          <Ionicons
+            name='search'
+            size={16}
+            color="#0EB676" />
         </TouchableOpacity>
-        <TouchableOpacity activeOpacity={.7} style={styles.appBarButton}>
-
+        <TouchableOpacity
+          activeOpacity={.7}
+          style={styles.appBarButton}>
+          <FontAwesome
+            name='sign-out'
+            size={16}
+            color="#0EB676" />
         </TouchableOpacity>
       </View>
       {/* END APPBAR */}
