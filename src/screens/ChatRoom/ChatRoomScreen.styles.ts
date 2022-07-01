beba8bcab389e226d850f3398b86ac29400921a0
huukhuong <huukhuong.it@gmail.com>
2022-07-01 17:06:20 +0700

    layout ChatRoomScreen

--- src/screens/ChatRoom/ChatRoomScreen.styles.ts
+++ src/screens/ChatRoom/ChatRoomScreen.styles.ts
@@ -1,7 +1,94 @@
 import { StyleSheet } from "react-native";
+import Colors from "../../utils/Themes";
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: Colors.DARK,
+  },
+  appBar: {
+    flexDirection: "row",
+    alignItems: "center",
+    height: 75,
+    paddingEnd: 24,
+    paddingStart: 8,
+    borderBottomWidth: .5,
+    borderBottomColor: Colors.LIGHT_3,
+  },
+  btnBack: {
+    width: 50,
+    height: 50,
+    borderRadius: 200,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  appBarAvatarWrapper: {
+    width: 40,
+    height: 40,
+    marginEnd: 8,
+  },
+  appBarAvatar: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    borderRadius: 200,
+  },
+  appBarOnline: {
+    position: "absolute",
+    backgroundColor: Colors.DARK,
+    borderRadius: 100,
+    bottom: 0,
+    right: 0,
+    paddingTop: .1,
+    paddingLeft: .5,
+  },
+  appBarTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: Colors.LIGHT,
+    flex: 1,
+  },
+  appBarButton: {
+    width: 40,
+    height: 40,
+    backgroundColor: Colors.LIGHT_GREEN,
+    marginStart: 10,
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  formGroup: {
+    flexDirection: "row",
+    position: "absolute",
+    left: 0,
+    bottom: 0,
+    right: 0,
+    paddingVertical: 18,
+    paddingHorizontal: 14,
+    borderTopWidth: .5,
+    borderTopColor: Colors.LIGHT_3,
+  },
+  input: {
+    flex: 1,
+    color: Colors.LIGHT,
+    backgroundColor: Colors.DARK_GREEN,
+    borderColor: Colors.LIGHT_3,
+    borderWidth: .6,
+    borderRadius: 14,
+    fontSize: 16,
+    maxHeight: 100,
+    paddingHorizontal: 12,
+    marginEnd: 10,
+  },
+  btnSend: {
+    backgroundColor: Colors.GREEN,
+    width: 50,
+    height: 50,
+    borderRadius: 18,
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  },
 });
 
 export default styles;
--- src/screens/ChatRoom/ChatRoomScreen.tsx
+++ src/screens/ChatRoom/ChatRoomScreen.tsx
@@ -2,14 +2,98 @@ import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParams } from "../../navigations/RootStackNavigation";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import styles from "./ChatRoomScreen.styles";
+import Colors from "../../utils/Themes";
+import Icon from "react-native-vector-icons/Ionicons";
+import Ripple from "react-native-material-ripple";
 
 type Props = NativeStackScreenProps<RootStackParams>;
 
 const ChatRoomScreen = ({ navigation, route }: Props) => {
   return (
-    <SafeAreaView>
-      <Text>{route.params?.uid}</Text>
+    <SafeAreaView style={styles.container}>
+      {/* APPBAR */}
+      <View style={styles.appBar}>
+
+        <Ripple
+          rippleColor={Colors.LIGHT_2}
+          style={styles.btnBack}
+        >
+          <Icon
+            name={"arrow-back-outline"}
+            size={24}
+            color={Colors.GREEN} />
+        </Ripple>
+
+        <View style={styles.appBarAvatarWrapper}>
+          <Image
+            source={{ uri: "https://cafefcdn.com/thumb_w/650/203337114487263232/2022/3/3/photo1646280815645-1646280816151764748403.jpg" }}
+            style={styles.appBarAvatar} />
+
+          <View style={styles.appBarOnline}>
+            <Icon
+              name={"ellipse"}
+              size={12}
+              color={Colors.GREEN} />
+          </View>
+        </View>
+
+        <Text
+          style={styles.appBarTitle}
+          numberOfLines={1}>
+          Adam Smith
+        </Text>
+
+        <TouchableOpacity
+          activeOpacity={.7}
+          style={styles.appBarButton}
+          onPress={() => {
+          }}>
+          <Icon
+            name="ios-call"
+            size={16}
+            color={Colors.GREEN} />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          activeOpacity={.7}
+          style={styles.appBarButton}
+          onPress={() => {
+          }}>
+          <Icon
+            name="ios-videocam"
+            size={16}
+            color={Colors.GREEN} />
+        </TouchableOpacity>
+
+      </View>
+      {/* END APPBAR */}
+
+      {/*  MESSAGES LIST */}
+
+      {/* END  MESSAGES LIST */}
+
+      {/*  FORM */}
+      <View style={styles.formGroup}>
+        <TextInput
+          style={styles.input}
+          placeholder={"Type message..."}
+          placeholderTextColor={Colors.LIGHT_3}
+          multiline
+        />
+        <View style={styles.btnSend}>
+          <Ripple
+            style={styles.btnSend}
+            rippleColor={Colors.LIGHT_1}>
+            <Icon
+              name={"ios-send"}
+              size={18}
+              color={Colors.LIGHT} />
+          </Ripple>
+        </View>
+      </View>
+      {/* END  FORM */}
     </SafeAreaView>
   );
 };
